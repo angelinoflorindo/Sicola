705400a50481b76500762bba5a23499cdb2810a9
angelino francisco <angelinoflorindo@gmail.com>
2026-03-16 11:38:12 +0100

    reparando o config

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -6,8 +6,8 @@
     port: Number(process.env.DB_PORT) || 3306,
 */
 export const options = {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
+    username: process.env.DB_USERNAME || 'usicola',
+    password: process.env.DB_PASSWORD || 'sic0La',
     database: process.env.DB_NAME || 'sicoladb',
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 3306,
