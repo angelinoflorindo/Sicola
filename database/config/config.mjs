3658496e8e7705ce28b7162eb5a340719de4f4c9
angelino francisco <angelinoflorindo@gmail.com>
2026-04-15 10:22:15 +0100

    corrigir erros do env

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -1,10 +1,10 @@
 
 export const options = {
-  username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'sicoladb',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     
     dialect: 'mysql',
     logging: process.env.NODE_ENV === "development" ? console.log : false,
