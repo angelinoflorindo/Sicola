90d302b6bb55f8e5680bd7908d6ef3b1e71f4cfd
angelino francisco <angelinoflorindo@gmail.com>
2026-04-28 03:01:17 +0100

    rever admin

--- database/seeders/20260118174445-seed-users.js
+++ database/seeders/20260118174445-seed-users.js
@@ -5,7 +5,8 @@
 const bcrypt = require("bcrypt");
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const hashed2 = await bcrypt.hash("999Flor", 12);
+    const hashed1 = await bcrypt.hash("999Flor", 12);
+    const hashed2 = await bcrypt.hash("999Chis", 12);
 
     await queryInterface.bulkInsert(
       "users",
@@ -14,10 +15,21 @@ module.exports = {
           primeiro_nome: "Angelino",
           segundo_nome: "Francisco",
           email: "240029@isaf.co.ao",
-          password: hashed2,
+          password: hashed1,
           // curso:'GBS',
           perfil: "ADMIN",
           telemovel: "930754775",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          primeiro_nome: "Rosalina",
+          segundo_nome: "Chissua",
+          email: "9281310@isaf.co.ao",
+          password: hashed2,
+          // curso:'GBS',
+          perfil: "ADMIN",
+          telemovel: "928131087",
           created_at: new Date(),
           updated_at: new Date(),
         },
